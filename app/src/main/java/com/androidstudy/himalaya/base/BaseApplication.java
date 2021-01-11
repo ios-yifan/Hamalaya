740d053e4da471f001b54feeeff2ce088ad57482
zhangyifan <zhangyifan@rongcloud.cn>
2021-01-11 20:14:22 +0800

    切换喜马拉雅 SDK

--- app/src/main/java/com/androidstudy/himalaya/base/BaseApplication.java
+++ app/src/main/java/com/androidstudy/himalaya/base/BaseApplication.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import com.androidstudy.himalaya.utils.LogUtils;
 import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
 import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.DeviceInfoProviderDefault;
 import com.ximalaya.ting.android.opensdk.player.XmPlayerManager;
 
 import java.util.logging.LogRecord;
@@ -20,22 +21,35 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+
+        //初始化播放器
+        XmPlayerManager.getInstance(this).init();
+
+
         CommonRequest mXimalaya = CommonRequest.getInstanse();
 
         if(DTransferConstants.isRelease) {
             String mAppSecret = "8646d66d6abe2efd14f2891f9fd1c8af";
             mXimalaya.setAppkey("9f9ef8f10bebeaa83e71e62f935bede8");
             mXimalaya.setPackid("com.app.test.android");
-            mXimalaya.init(this,mAppSecret);
+            mXimalaya.init(this ,mAppSecret, new DeviceInfoProviderDefault(this) {
+                @Override
+                public String oaid() {
+                    return "!!!这里要传入真正的oaid oaid 接入请访问 http://www.msa-alliance.cn/col.jsp?id=120";
+                }});
         } else {
             String mAppSecret = "0a09d7093bff3d4947a5c4da0125972e";
             mXimalaya.setAppkey("f4d8f65918d9878e1702d49a8cdf0183");
             mXimalaya.setPackid("com.ximalaya.qunfeng");
-            mXimalaya.init(this,mAppSecret);
+                mXimalaya.init(this ,mAppSecret, new DeviceInfoProviderDefault(this) {
+                    @Override
+                    public String oaid() {
+                        return "!!!这里要传入真正的oaid oaid 接入请访问 http://www.msa-alliance.cn/col.jsp?id=120";
+                    }});
+
         }
 
-        //初始化播放器
-        XmPlayerManager.getInstance(this).init();
 
         /*日志初始化*/
         LogUtils.init(this.getPackageName(),false);
